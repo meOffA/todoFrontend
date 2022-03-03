4d99adb11c74d2c89eae952dfe577eed2a108730
meoffAdam <adamnow96@gmail.com>
2022-03-03 23:06:23 +0100

    test1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
